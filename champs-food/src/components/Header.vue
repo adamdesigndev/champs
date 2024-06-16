00a0df1491695c5b3a4a8def18bda4982f2cb520
Adam <adamdesigndev@gmail.com>
2024-06-15 20:09:13 -0700

    Primary CSS setup complete

--- champs-food/src/components/Header.vue
+++ champs-food/src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
     <header>
-      <nav>
-        <ul>
+      <nav class="wrapper-nav-inner">
+        <ul class="nav-menu-links">
           <li><a href="/">Home</a></li>
           <li><a href="/menu">Menu</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/cart">Cart ({{ cartItemCount }})</a></li>
+        </ul>
+        <img src="" alt="">
+        <ul>
+          <li><button></button></li>
+          <li><a href="/cart"><img src="" alt=""></a></li>
         </ul>
       </nav>
     </header>
@@ -18,25 +22,6 @@
   </script>
   
   <style scoped>
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    background-color: #ffcc00;
-  }
-  
-  nav ul {
-    display: flex;
-    list-style: none;
-  }
-  
-  nav li {
-    margin: 0 1rem;
-  }
-  
-  nav a {
-    text-decoration: none;
-    color: black;
-  }
+
+
   </style>
