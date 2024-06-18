9f5a08867e7e9f29add35b6a0a54e93cece2c859
Adam <adamdesigndev@gmail.com>
2024-06-18 00:18:28 -0700

    Updated hero

--- champs-food/src/components/HeroSection.vue
+++ champs-food/src/components/HeroSection.vue
@@ -1,11 +1,14 @@
 <template>
-    <section class="hero">
-      <div class="hero-content">
-        <h1>Welcome to Fast Food Restaurant</h1>
-        <p>Delicious food, ready to serve!</p>
+    <section class="wrapper-outer-hero-home">
+      <div class="wrapper-inner-hero-content">
+        <div class="wrapper-inner-hero-left">
+          <h1 class="header-1">Welcome Champs</h1>
+        <p class="body-bottom-button">Delicious food, ready to serve!</p>
+        <button class="main-btn">Start Order</button>
+        </div>
+        <div class="wrapper-inner-hero-right">
+        <img class="two-up-section-image" src="/images/grilledClub_colbyJack_PDP.png" alt="Delicious food">
       </div>
-      <div class="hero-image">
-        <img src="" alt="Delicious food">
       </div>
     </section>
   </template>
@@ -14,16 +17,18 @@
   </script>
   
   <style scoped>
-  .hero {
+  .wrapper-outer-hero-home {
+    background-color: var(--clr-accent-creme);
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: var(--primary-color);
-    padding: 2rem;
+    justify-content: center;
+    padding: var(--wrapper-outer-padding-tb);
   }
-  
-  .hero-content {
-    flex: 1;
+
+  .wrapper-inner-hero-content {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: var(--content-gap);
+    max-width: var(--content-max-width-v1);
   }
   
   .hero-image img {
