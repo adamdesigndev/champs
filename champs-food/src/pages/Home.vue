8c59cfb44292ec4eb6bbcabedceb715774c03c16
Adam <adamdesigndev@gmail.com>
2024-06-18 15:15:08 -0700

    Styles added to two up component

--- champs-food/src/pages/Home.vue
+++ champs-food/src/pages/Home.vue
@@ -2,10 +2,12 @@
     <div>
       <HeroSection />
       <ThreeColumnSection />
+      <BasicTwoColumn />
     </div>
   </template>
   
   <script setup>
   import HeroSection from '../components/HeroSection.vue';
   import ThreeColumnSection from '../components/ThreeColumnSection.vue';
+  import BasicTwoColumn from '../components/BasicTwoColumn.vue';
   </script>
