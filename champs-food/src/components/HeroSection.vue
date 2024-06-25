e67a1235040008ae6f4dd1278755de7007fe3dae
Adam <adamdesigndev@gmail.com>
2024-06-25 16:52:45 -0700

    Setup props for Hero

--- champs-food/src/components/HeroSection.vue
+++ champs-food/src/components/HeroSection.vue
@@ -1,38 +1,38 @@
+<!-- HeroSection.vue -->
 <template>
-    <section class="wrapper-outer-hero-home">
-      <div class="wrapper-inner-hero-content">
-        <div class="wrapper-inner-hero-left">
-          <h1 class="header-1">Welcome Champs</h1>
-        <p class="body-bottom-button">Delicious food, ready to serve!</p>
-        <button class="main-btn">Start Order</button>
-        </div>
-        <div class="wrapper-inner-hero-right">
-        <img class="two-up-section-image" src="/images/grilledClub_colbyJack_PDP.png" alt="Delicious food">
+  <section class="wrapper-outer-hero-home">
+    <div class="wrapper-inner-hero-content wrapper-general">
+      <div class="wrapper-inner-hero-left">
+        <h1 class="header-1">{{ heading }}</h1>
+      <p class="body-bottom-button">{{ subheading }}</p>
+      <button class="main-btn"><a class="" href="buttonLink">{{ buttonText }}</a></button>
       </div>
-      </div>
-    </section>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  .wrapper-outer-hero-home {
-    background-color: var(--clr-accent-creme);
-    display: flex;
-    justify-content: center;
-    padding: var(--wrapper-outer-padding-tb);
-  }
+      <div class="wrapper-inner-hero-right">
+      <img class="two-up-section-image" src="/images/grilledClub_colbyJack_PDP.png" alt="Delicious food">
+    </div>
+    </div>
+  </section>
+</template>
 
-  .wrapper-inner-hero-content {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: var(--content-gap);
-    max-width: var(--content-max-width-v1);
-  }
-  
-  .hero-image img {
-    width: 100%;
-    max-width: 400px;
-  }
-  </style>
+<script setup>
+</script>
+
+<style scoped>
+.wrapper-outer-hero-home {
+  background-color: var(--clr-accent-creme);
+  display: flex;
+  justify-content: center;
+  padding: var(--wrapper-outer-padding-tb);
+}
+
+.wrapper-inner-hero-content {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: var(--content-gap);
+}
+
+.hero-image img {
+  width: 100%;
+  max-width: 400px;
+}
+</style>
