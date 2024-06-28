38ee4fd2a12e539a6f445d3dca62f3194bb0f9d9
Adam <adamdesigndev@gmail.com>
2024-06-28 00:05:05 -0700

    Adding products to data.js

--- champs-food/src/data.js
+++ champs-food/src/data.js
@@ -1,43 +1,149 @@
 export const foodItems = [
+  // Drinks
     {
-      name: 'Chicken Tender',
-      description: 'Very juicy chicken strips',
+      name: 'Coca-Cola®',
+      description: 'The Original Coca-Cola® crisp & refreshing.',
       sizes: {
-        small: 5.99,
-        medium: 6.99,
-        large: 7.99
+        small: 0.99,
+        medium: 1.09,
+        large: 1.19
       },
       image: '/images/chicken-tender.jpg',
       featured: false,
-      category: 'chicken'
+      category: 'Beverage'
     },
     {
-      name: 'Spicy Chicken Burger',
-      description: 'Very spicy chicken burger',
-      price: 5.99,
-      image: '/images/chicken-spicy-burger.jpg',
-      featured: true,
-      category: 'burger'
-    },
-    {
-      name: 'Spicy Chicken Burger',
-      description: 'Very spicy chicken burger',
-      price: 5.99,
-      image: '/images/chicken-spicy-burger.jpg',
-      featured: true,
-      category: 'burger'
-    },
-    {
-      name: 'Chicken Tender',
-      description: 'Very juicy chicken strips',
+      name: 'Sprite®',
+      description: 'Lemon-lime soda with 100% natural flavors.',
       sizes: {
-        small: 5.99,
-        medium: 6.99,
-        large: 7.99
+        small: 0.99,
+        medium: 1.09,
+        large: 1.19
       },
       image: '/images/chicken-tender.jpg',
       featured: false,
-      category: 'chicken'
+      category: 'Beverage'
+    },
+    {
+      name: 'Minute Maid® Lemonade',
+      description: 'Classic lemonade with a sweet, refreshing taste.',
+      sizes: {
+        small: 0.99,
+        medium: 1.09,
+        large: 1.19
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Beverage'
+    },
+    {
+      name: 'Dr Pepper®',
+      description: 'Cool off with a refreshing Dr Pepper®.',
+      sizes: {
+        small: 0.99,
+        medium: 1.09,
+        large: 1.19
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Beverage'
+    },
+    {
+      name: 'Simply Orange®',
+      description: '100% pure-squeezed pasteurized orange juice.',
+      sizes: {
+        small: 0.99,
+        medium: 1.09,
+        large: 1.19
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Beverage'
+    },
+    {
+      name: 'Fanta® ',
+      description: 'Tingly, fruity taste that is bright and bubbly.',
+      sizes: {
+        small: 0.99,
+        medium: 1.09,
+        large: 1.19
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Beverage'
+    },
+    // Desserts
+    {
+      name: 'Oreo® Shake',
+      description: 'Classic OREO® cookies pieces make this thick and creamy shake one of the best ways to have your milk and cookies.',
+      sizes: {
+        small: 3.99,
+        medium: 4.99,
+        large: 5.99
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Dessert'
+    },
+    {
+      name: 'Pineapple Express Shake',
+      description: 'Sip, sip, relax with this thick & creamy shake with pineapple flavors.',
+      sizes: {
+        small: 3.99,
+        medium: 4.99,
+        large: 5.99
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Dessert'
+    },
+    {
+      name: 'Vanilla Shake w/ Boba',
+      description: 'A thick and creamy Vanilla Shake made even more delicious with brown sugar Boba and a whipped topping.',
+      sizes: {
+        small: 3.99,
+        medium: 4.99,
+        large: 5.99
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Dessert'
+    },
+    {
+      name: 'Strawberry Shake',
+      description: 'A thick and creamy shake, blended with strawberry flavors. Worth getting brain freeze over.',
+      sizes: {
+        small: 3.99,
+        medium: 4.99,
+        large: 5.99
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Dessert'
+    },
+    {
+      name: 'Chocolate Shake',
+      description: 'Our thick and creamy shake with chocolate syrup is a timeless classic. Get it before it is gone. Just kidding, it is not going anywhere.',
+      sizes: {
+        small: 3.99,
+        medium: 4.99,
+        large: 5.99
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Dessert'
+    },
+    {
+      name: 'Vanilla Shake',
+      description: 'A thick and creamy vanilla shake. It is old-school cool.',
+      sizes: {
+        small: 3.99,
+        medium: 4.99,
+        large: 5.99
+      },
+      image: '/images/chicken-tender.jpg',
+      featured: false,
+      category: 'Dessert'
     },
     {
       name: 'Spicy Chicken Burger',
@@ -47,12 +153,5 @@ export const foodItems = [
       featured: true,
       category: 'burger'
     },
-    {
-      name: 'Spicy Chicken Burger',
-      description: 'Very spicy chicken burger',
-      price: 5.99,
-      image: '/images/chicken-spicy-burger.jpg',
-      featured: true,
-      category: 'burger'
-    }
+ 
   ];
