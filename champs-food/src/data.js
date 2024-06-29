aed487991245d576a65deb17688a0a111e4d4b86
Adam <adamdesigndev@gmail.com>
2024-06-28 20:21:34 -0700

    Added product images

--- champs-food/src/data.js
+++ champs-food/src/data.js
@@ -8,7 +8,7 @@ export const foodItems = [
         medium: 1.09,
         large: 1.19
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Coca-Cola.png',
       featured: false,
       category: 'Beverage'
     },
@@ -20,7 +20,7 @@ export const foodItems = [
         medium: 1.09,
         large: 1.19
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Sprite.png',
       featured: false,
       category: 'Beverage'
     },
@@ -32,7 +32,7 @@ export const foodItems = [
         medium: 1.09,
         large: 1.19
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Lemonade.png',
       featured: false,
       category: 'Beverage'
     },
@@ -44,7 +44,7 @@ export const foodItems = [
         medium: 1.09,
         large: 1.19
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Dr_Pepper.png',
       featured: false,
       category: 'Beverage'
     },
@@ -56,7 +56,7 @@ export const foodItems = [
         medium: 1.09,
         large: 1.19
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/SimplyOrange.png',
       featured: false,
       category: 'Beverage'
     },
@@ -68,7 +68,7 @@ export const foodItems = [
         medium: 1.09,
         large: 1.19
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Fanta.png',
       featured: false,
       category: 'Beverage'
     },
@@ -81,7 +81,7 @@ export const foodItems = [
         medium: 4.99,
         large: 5.99
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Oreo-Shake.png',
       featured: false,
       category: 'Dessert'
     },
@@ -93,7 +93,7 @@ export const foodItems = [
         medium: 4.99,
         large: 5.99
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Pineapple-Express-Shake.png',
       featured: false,
       category: 'Dessert'
     },
@@ -105,7 +105,7 @@ export const foodItems = [
         medium: 4.99,
         large: 5.99
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Vanilla-Shake-Boba.png',
       featured: false,
       category: 'Dessert'
     },
@@ -117,7 +117,7 @@ export const foodItems = [
         medium: 4.99,
         large: 5.99
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Strawberry-Shake.png',
       featured: false,
       category: 'Dessert'
     },
@@ -129,7 +129,7 @@ export const foodItems = [
         medium: 4.99,
         large: 5.99
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Chocolate-Shake.png',
       featured: false,
       category: 'Dessert'
     },
@@ -141,17 +141,194 @@ export const foodItems = [
         medium: 4.99,
         large: 5.99
       },
-      image: '/images/chicken-tender.jpg',
+      image: '/images/Vanilla-Shake.png',
       featured: false,
       category: 'Dessert'
     },
+    // Sandwiches
     {
-      name: 'Spicy Chicken Burger',
-      description: 'Very spicy chicken burger',
+      name: 'HOMESTYLE RANCH CHICKEN CLUB',
+      description: '100% all-white meat crispy chicken fillet with melty Swiss style cheese, hickory smoked bacon, lettuce, tomato & buttermilk ranch on a toasted brioche bun.',
       price: 5.99,
-      image: '/images/chicken-spicy-burger.jpg',
+      image: '/images/Homestyle-Ranch-Chicken-Club.png',
       featured: true,
-      category: 'burger'
+      category: 'Sandwich'
     },
- 
+    {
+      name: 'SPICY CHICKEN® W/ CHEESE',
+      description: '100% all white meat spicy crispy chicken fillet with melty Swiss style cheese, tomato, lettuce & mayo on a toasted buttery bakery bun.',
+      price: 5.99,
+      image: '/images/Spicy-Chicken-W-Cheese.png',
+      featured: false,
+      category: 'Sandwich'
+    },
+    {
+      name: 'SPICY CHICKEN',
+      description: 'A boneless breast of chicken seasoned with a spicy blend of peppers, freshly breaded, pressure cooked in 100% refined peanut oil and served on a toasted, buttered bun with dill pickle chips. ',
+      price: 5.99,
+      image: '/images/Spicy-Chicken.png',
+      featured: true,
+      category: 'Sandwich'
+    },
+    {
+      name: 'CRISPY CHICKEN STRIPS',
+      description: 'Boneless chicken tenders seasoned to perfection, freshly breaded and pressure cooked in 100% refined peanut oil.',
+      price: 5.99,
+      image: '/images/Crispy-Chicken-Strips.png',
+      featured: false,
+      category: 'Sandwich'
+    },
+    {
+      name: 'CLUCK SANDWICH',
+      description: '100% all-white meat crispy chicken fillet with pickles & sauce on a toasted brioche bun. There is nothing not awesome about this.',
+      price: 5.99,
+      image: '/images/Cluck-Sandwich.png',
+      featured: false,
+      category: 'Sandwich'
+    },
+    {
+      name: 'GRILLED CHICKEN SANDWICH',
+      description: 'A lemon-herb marinated boneless breast of chicken, grilled for a tender and juicy backyard-smoky taste, served on a toasted multigrain bun with green leaf lettuce and tomato.',
+      price: 5.99,
+      image: '/images/Grilled-Chicken-Sandwich.png',
+      featured: false,
+      category: 'Sandwich'
+    },
+    // Salads
+    {
+      name: 'GARDEN SALAD W/ GRILLED CHICKEN',
+      description: 'Mixed greens topped with grilled chicken strips, grape tomatoes, shredded carrots, shredded cheddar, croutons & buttermilk ranch dressing.',
+      image: '/images/Garden-Salad-Grilled-Chicken.png',
+      featured: false,
+      category: 'Salad'
+    },
+    {
+      name: 'GARDEN SALAD W/ CRISPY CHICKEN',
+      description: 'Mixed greens topped with crispy chicken strips, grape tomatoes, shredded carrots, shredded cheddar, croutons & buttermilk ranch dressing.',
+      image: '/images/Garden-Salad-Crispy-Chicken.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'SOUTHWEST SALAD W/ GRILLED CHICKEN',
+      description: 'Mixed greens with grilled chicken strips, tomatoes, shredded carrots, shredded cheddar, roasted corn, black beans, tortilla strips & Southwest dressing.',
+      image: '/images/SW-Salad-Grilled-Chicken.png',
+      featured: false,
+      category: 'Salad'
+    },
+    {
+      name: 'SOUTHWEST SALAD W/ CRISPY CHICKEN',
+      description: 'Mixed greens with crispy chicken strips, tomatoes, shredded carrots, shredded cheddar, roasted corn, black beans, tortilla strips & Southwest dressing.',
+      image: '/images/SW-Salad-Crispy-Chicken.png',
+      featured: false,
+      category: 'Salad'
+    },
+    {
+      name: 'SIDE SALAD',
+      description: 'Crisp iceberg and romaine lettuce, grape tomatoes & carrots with Champs Famous Buttermilk Ranch dressing. Champs Buttermilk Ranch on a salad? Sign me up.',
+      image: '/images/Side-Salad.png',
+      featured: false,
+      category: 'Salad'
+    },
+    // Sides
+    {
+      name: 'FRENCH FRIES',
+      description: 'Potatoe fries cooked in canola oil until crispy outside and tender inside. Sprinkled with Sea Salt.',
+      sizes: {
+        small: 1.99,
+        medium: 2.99,
+        large: 3.99
+      },
+      image: '/images/French-Fry.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'CURLY FRIES',
+      description: 'Perfectly seasoned, crispy curly fries..',
+      sizes: {
+        small: 1.99,
+        medium: 2.99,
+        large: 3.99
+      },
+      image: '/images/Curly-Fry.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'ONION RINGS',
+      description: 'Panko breaded and fried to perfection..',
+      sizes: {
+        small: 1.99,
+        medium: 2.99,
+        large: 3.99
+      },
+      image: '/images/Onion-Rings.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'CLASSIC GRILLED WRAP',
+      description: 'Grilled chicken strips, shredded lettuce, shredded cheddar, jalapenos, Spicy Good Good sauce & buttermilk ranch in a warm tortilla.',
+      image: '/images/Classic-Grilled-Wrap.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'CLASSIC CRISPY WRAP',
+      description: 'A chicken strip, shredded lettuce, shredded cheddar, pickles, Good Good sauce & buttermilk ranch dressing wrapped up in a warm tortilla.',
+      image: '/images/Classic-Crispy-Wrap.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'SPICY GRILLED WRAP ',
+      description: 'Grilled chicken strips, shredded lettuce, shredded cheddar, jalapenos, Spicy Good Good sauce & buttermilk ranch in a warm tortilla.',
+      image: '/images/Spicy-Grilled-Wrap.png',
+      featured: false,
+      category: 'Side'
+    },
+    // Breakfast
+    {
+      name: 'Chicken Biscuit',
+      description: 'A breakfast portion of our famous boneless breast of chicken, seasoned to perfection, hand-breaded, pressure cooked in 100% refined peanut oil and served on a buttermilk biscuit baked fresh at each Restaurant.',
+      image: '/images/Biscuit-Chicken.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'Egg White Grill',
+      description: 'A breakfast portion of grilled chicken with a hint of citrus, served on a toasted classic English Muffin with egg whites and American cheese.',
+      image: '/images/eggWHTchk.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'Bacon, Egg & Cheese Muffin',
+      description: 'Delicious strips of smoked applewood bacon, folded egg and American cheese served on a toasted classic English Muffin.',
+      image: '/images/BaconEggCheeseMuffin.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'Chicken, Egg & Cheese Biscuit',
+      description: 'A breakfast portion of our famous boneless breast of chicken, seasoned to perfection, freshly breaded, pressure cooked in 100% refined peanut oil, a folded egg and cheese, served on a buttermilk biscuit baked fresh at each Restaurant.',
+      image: '/images/Biscuit_ChickenEggCheese.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'Small Hash Browns',
+      description: 'Crispy potato medallions cooked in canola oil.',
+      image: '/images/Breakfast-Hashbrowns.png',
+      featured: false,
+      category: 'Side'
+    },
+    {
+      name: 'Hash Brown Scramble Bowl',
+      description: 'A hearty morning meal of sliced Chick-fil-A Nuggets, crispy Hash Browns, scrambled eggs and a blend of Monterey Jack and Cheddar cheeses. Made fresh each morning. Served in a convenient bowl. Served with Jalapeño Salsa.',
+      image: '/images/ChickenHashbrownBreakfastBowl.png',
+      featured: false,
+      category: 'Side'
+    },
   ];
