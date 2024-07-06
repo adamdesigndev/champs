3563f34bb07fb9f962a963fb3a2362b2a0b65c56
Adam <adamdesigndev@gmail.com>
2024-07-05 18:01:15 -0700

    Home page udates

--- champs-food/src/components/BasicTwoColumn.vue
+++ champs-food/src/components/BasicTwoColumn.vue
@@ -1,15 +1,31 @@
 <!-- @format -->
-
 <!-- BasicTwoColumn.vue -->
 <template>
   <section :class="['wrapper-basic-outer-section', { 'special-two-up': red }]">
     <div class="wrapper-general">
       <div :class="['wrapper-basic-two-up', { 'wrapper-reverse': reverse }]">
         <div class="wrapper-inner-left">
-          <h2 class="header-2">{{ header }}</h2>
-          <p class="main-copy body-bottom-button">{{ body }}</p>
-          <a class="" :href="buttonLink"
-            ><button class="main-btn">{{ buttonText }}</button></a
+          <h2 :class="['header-2', { 'white-text': textWhite }]">
+            {{ header }}
+          </h2>
+          <img
+            v-if="showAppStore"
+            class="app-store"
+            src="/images/app-store.svg"
+            alt="App Store"
+          />
+          <p
+            :class="[
+              'main-copy body-bottom-button',
+              { 'white-text': textWhite },
+            ]"
+          >
+            {{ body }}
+          </p>
+          <a :href="buttonLink"
+            ><button :class="['main-btn', { 'button-reverse': buttonReverse }]">
+              {{ buttonText }}
+            </button></a
           >
         </div>
         <div class="wrapper-inner-right">
@@ -36,25 +52,52 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  textWhite: {
+    type: Boolean,
+    default: false,
+  },
+  buttonReverse: {
+    type: Boolean,
+    default: false,
+  },
+  showAppStore: {
+    type: Boolean,
+    default: false,
+  },
 });
 </script>
 
 <style scoped>
 .wrapper-basic-two-up {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
   gap: 2rem;
 }
 
-.wrapper-reverse .wrapper-inner-hero-left {
+.wrapper-reverse .wrapper-inner-left {
   order: 2;
 }
 
-.wrapper-reverse .wrapper-inner-hero-right {
+.wrapper-reverse .wrapper-inner-right {
   order: 1;
 }
 
 .special-two-up {
   background-color: var(--clr-primary);
+}
+
+.white-text {
+  color: #fff;
+}
+
+.button-reverse {
+  background-color: #fff;
+  color: var(--clr-primary);
+  font-weight: 600;
+}
+
+.app-store {
+  max-width: 300px;
+  margin-bottom: 1rem;
 }
 </style>
--- champs-food/src/pages/Home.vue
+++ champs-food/src/pages/Home.vue
@@ -4,22 +4,50 @@
 <template>
   <div>
     <HeroSection
-      heading="Chicken for the Champs"
-      subheading="Delicious food, ready to serve!"
-      buttonText="Add To Order"
-      buttonLink="/SingleFoodItem"
-      imageSrc="/images/grilledClub_colbyJack_PDP.png"
-      imageAlt="Delicous food"
+      :heading="heroHeading"
+      :subheading="heroSubheading"
+      :buttonText="heroButtonText"
+      :buttonLink="heroButtonLink"
+      :imageSrc="heroImageSrc"
+      :imageAlt="heroImageAlt"
     />
     <ThreeColumnSection />
     <BasicTwoColumn
-      imageSrc="/images/DALL·E 2024-06-06 20.35.05 - A fast food restaurant employee, a female wearing a red uniform and cap, serving food to a family. The focus is more on the female employee, who is ha.webp"
-      imageAlt=""
-      header="Who we are"
-      body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-      buttonText="Learn More"
-      buttonLink="/About"
-      reverse="false"
+      :imageSrc="firstTwoColumnImageSrc"
+      :imageAlt="firstTwoColumnImageAlt"
+      :header="firstTwoColumnHeader"
+      :body="firstTwoColumnBody"
+      :buttonText="firstTwoColumnButtonText"
+      :buttonLink="firstTwoColumnButtonLink"
+      :reverse="true"
+      :red="false"
+      :textWhite="false"
+      :buttonReverse="false"
+    />
+    <BasicTwoColumn
+      :imageSrc="secondTwoColumnImageSrc"
+      :imageAlt="secondTwoColumnImageAlt"
+      :header="secondTwoColumnHeader"
+      :body="secondTwoColumnBody"
+      :buttonText="secondTwoColumnButtonText"
+      :buttonLink="secondTwoColumnButtonLink"
+      :reverse="false"
+      :red="true"
+      :textWhite="true"
+      :buttonReverse="true"
+    />
+    <BasicTwoColumn
+      :imageSrc="thirdTwoColumnImageSrc"
+      :imageAlt="thirdTwoColumnImageAlt"
+      :header="thirdTwoColumnHeader"
+      :body="thirdTwoColumnBody"
+      :buttonText="thirdTwoColumnButtonText"
+      :buttonLink="thirdTwoColumnButtonLink"
+      :reverse="true"
+      :red="false"
+      :textWhite="false"
+      :buttonReverse="false"
+      :showAppStore="true"
     />
   </div>
 </template>
@@ -28,4 +56,34 @@
 import HeroSection from "../components/HeroSection.vue";
 import ThreeColumnSection from "../components/ThreeColumnSection.vue";
 import BasicTwoColumn from "../components/BasicTwoColumn.vue";
+
+const heroHeading = "Chicken for the Champs";
+const heroSubheading = "Delicious food, ready to serve!";
+const heroButtonText = "Add To Order";
+const heroButtonLink = "/SingleFoodItem";
+const heroImageSrc = "/images/Homestyle-Ranch-Chicken-Club.png";
+const heroImageAlt = "Delicious food";
+
+const firstTwoColumnImageSrc =
+  "/images/DALL·E 2024-06-06 20.35.05 - A fast food restaurant employee, a female wearing a red uniform and cap, serving food to a family. The focus is more on the female employee, who is ha.webp";
+const firstTwoColumnImageAlt = "";
+const firstTwoColumnHeader = "Who we are";
+const firstTwoColumnBody =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+const firstTwoColumnButtonText = "Learn More";
+const firstTwoColumnButtonLink = "/About";
+
+const secondTwoColumnImageSrc = "/images/Grilled-Chicken-Sandwich.png";
+const secondTwoColumnImageAlt = "";
+const secondTwoColumnHeader = "Bigger, Juicier and bolder than ever before";
+const secondTwoColumnBody = "";
+const secondTwoColumnButtonText = "Get It Now";
+const secondTwoColumnButtonLink = "/Services";
+
+const thirdTwoColumnImageSrc = "/images/Champs-app.png";
+const thirdTwoColumnImageAlt = "";
+const thirdTwoColumnHeader = "Free Spicy Chicken w/ $1 min. purchase";
+const thirdTwoColumnBody = "Vist the app store for more deals and offers!";
+const thirdTwoColumnButtonText = "Join Rewards";
+const thirdTwoColumnButtonLink = "/Contact";
 </script>
